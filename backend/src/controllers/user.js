a556be95fd33b9ef27ae15501ec3e81612ab2277
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2025-01-12 16:17:29 +0300

    list added

---
+++ backend/src/controllers/user.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const User = require("../models/user");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(User);
+
+    res.status(200).send({
+      error: false,
+      count: data.length,
+      data: data,
+    });
+  },
+
+  
+};
