f26d08cee1566c7916445b1affa37beea5f6f064
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2025-01-13 15:10:19 +0300

    comment model added

---
+++ backend/src/models/comments.js
@@ -0,0 +1,30 @@
+"use strict";
+// -------------------------
+// CAPSTONE BLOG API
+// -------------------------
+
+const { mongoose } = require("../configs/dbConnection");
+// -------------------------
+const CommentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    blogId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Blog",
+      required: true,
+    },
+    comment: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  { collection: "comments", timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
