6dc8f2a1ea0460552eca71ec55a9fdbe5018165b
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2025-02-07 16:16:30 +0300

    pageination error fix

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,71 +1,70 @@
-
 function Pagination({ details }) {
-
-
   return (
     <>
-      <nav aria-label="Page navigation example">
-        <ul className="flex items-center -space-x-px h-8 text-sm">
-          <li>
-            <a
-              href={`/blogs?page=${details?.pages?.previous}`}
-              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
-            >
-              <span className="sr-only">Previous</span>
-              <svg
-                className="w-2.5 h-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-            </a>
-          </li>
-
-          {details?.pages?.list?.map((page, index) => (
-            <li key={index}>
+      {details?.pages?.total > 1 && (
+        <nav aria-label="Page navigation example">
+          <ul className="flex items-center -space-x-px h-8 text-sm">
+            <li>
               <a
-                href={`/blogs?page=${page}`}
-                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                href={`/blogs?page=${details?.pages?.previous}`}
+                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
               >
-                {page}
+                <span className="sr-only">Previous</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 1 1 5l4 4"
+                  />
+                </svg>
               </a>
             </li>
-          ))}
 
-          <li>
-            <a
-              href={`/blogs?page=${details?.pages?.next}`}
-              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
-            >
-              <span className="sr-only">Next</span>
-              <svg
-                className="w-2.5 h-2.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
+            {details?.pages?.list?.map((page, index) => (
+              <li key={index}>
+                <a
+                  href={`/blogs?page=${page}`}
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  {page}
+                </a>
+              </li>
+            ))}
+
+            <li>
+              <a
+                href={`/blogs?page=${details?.pages?.next}`}
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </nav>
+                <span className="sr-only">Next</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </>
   );
 }
